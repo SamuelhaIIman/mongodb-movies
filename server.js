6e670fc33d4b30245031c91b59eeca9f65b6ac34
Samuel Hållman <samuel.hollman@hotmail.com>
2025-02-05 19:25:11 +0200

    Testing done with ejs and MongoDB

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
-let MongoClient = require('mongodb').MongoClient;
-let url = "mongodb://localhost:27017/";
+const express = require("express");
+const { MongoClient } = require("mongodb");
 
-MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    let dbo = db.db("mydb");
-    dbo.collection("movies").findOne({}, function(err, result) {
-        if (err) throw err;
-        console.log(result.name);
-        db.close();
-    });
-}); 
+const app = express();
+const port = 3000;
+const url = "mongodb://localhost:27017/";
+const dbName = "mydb";
+
+app.set("view engine", "ejs");
+
+// Connect to MongoDB and fetch movies
+app.get("/", async (req, res) => {
+    try {
+        const client = new MongoClient(url);
+        await client.connect();
+        const db = client.db(dbName);
+        const movies = await db.collection("movies").find().toArray();
+        await client.close();
+
+        res.render("index", { movies });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Error retrieving data");
+    }
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
